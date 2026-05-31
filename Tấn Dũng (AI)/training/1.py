0382a09462d5e5c488ce82770b1797931f66332e
Dung Nguyen <nguyendung16042003@gmail.com>
2026-05-31 20:59:48 +0700

    oke

--- Tấn Dũng (AI)/training/1.py
+++
@@ -1,8 +0,0 @@
-# conda activate fall
-from ultralytics import YOLO
-
-model = YOLO("yolov8n.pt")
-
-results = model(r"D:\DOWLOAD\FileTaiLieuHocTapCuaDung\Ki9\Đồ án\Fall Detection\Datasets\FA\train\images\-Flashback-Man-falls-and-slips-down-icy-driveway-Winter-fail-compilation-_-Laugh-Down_46_jpg.rf.7NCRbkM7iuFuoaruHwp1.jpg")
-
-print("YOLO hoạt động bình thường")
